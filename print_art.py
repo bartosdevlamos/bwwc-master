48f56819314db75ed7dc84ddeb61cec5b889424b
bartosdevlamos <bartosdevlamos@gmail.com>
2024-07-09 21:08:27 +0200

    Create print_art.py

---
+++ print_art.py
@@ -0,0 +1,31 @@
+def print_bwwc_art():
+    art = """
+  ____            __        __      ____ ___ 
+ | __ )   _  __   \\ \\      / /     | __ )_ _|
+ |  _ \\  | |/ /    \\ \\ /\\ / /      |  _ \\| | 
+ | |_) | |   <      \\ V  V /       | |_) | | 
+ |____/  |_|\\_\\      \\_/\\_/        |____/___|
+                                             
+   Baabs    Wide    Web             Crawler 
+   
+This script is designed to crawl web pages starting from a given URL, 
+collect all the unique domain names it finds, and save them to an Excel file.
+"""
+    print(art)
+
+disclaimer = """
+
+Disclaimer:
+
+This web crawling tool (baab.py) is provided for educational and informational purposes only. By using this tool, you agree that:
+
+- The tool is used at your own risk.
+- I, as the creator of this tool, do not take responsibility for any damages or consequences that may arise from its use.
+- It is your responsibility to use this tool in accordance with applicable laws and ethical considerations.
+- This tool may interact with websites and servers; therefore, misuse or unauthorized use of this tool may violate terms of service or legal agreements with those websites.
+
+Use this tool responsibly and respect the rights and privacy of others. If you do not agree with these terms, do not use the tool.
+"""
+
+def print_disclaimer():
+    print(disclaimer)
